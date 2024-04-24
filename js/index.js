3670f8d8808141deef568d70bd3c9038adf045d4
MMartindale3 <145407645+MMartindale3@users.noreplay.github.com>
2024-04-23 20:45:22 -0600

    Task 1 complete

--- js/index.js
+++ js/index.js
@@ -1,7 +1,32 @@
 async function appInit() {
     const response = await fetch("https://661897109a41b1b3dfbd735b.mockapi.io/albums");
-    const fetchData = await response.json();
-    console.log(fetchData);    
+    const fetchData = await response.json();   
+}
+appInit();
+
+const toFavouritesButton = document.querySelector("#favorites-button");
+const toSearchButton = document.querySelector("#search-button");
+const favouritesTab = document.querySelector("#favorites-tab");
+const searchTab = document.querySelector("#search-tab");
+
+
+
+toFavouritesButton.addEventListener("click", onSwitchToFavourites);
+function onSwitchToFavourites(e) {
+    e.preventDefault();
+    toSearchButton.classList.remove("active");
+    toFavouritesButton.classList.add("active");
+    favouritesTab.classList.remove("d-none");
+    searchTab.classList.add("d-none");
+    console.log("switched to favourites");    
 }
 
-appInit();
+toSearchButton.addEventListener("click", onSwitchToSearch);
+function onSwitchToSearch(e) {
+    e.preventDefault();
+    toSearchButton.classList.add("active");
+    toFavouritesButton.classList.remove("active");
+    favouritesTab.classList.add("d-none");
+    searchTab.classList.remove("d-none");
+    console.log("switched to search");
+}
